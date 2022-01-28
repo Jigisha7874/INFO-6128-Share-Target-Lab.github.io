92f9df10f7fcec4171c3111e537ffef251bac54f
Jigisha7874 <80816715+Jigisha7874@users.noreply.github.com>
2022-01-27 19:04:14 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
     const parsedUrl = new URL(window.location);
     // searchParams.get() will properly handle decoding the values.
-    document.getElementById('title').innerHTML = "value";
-    document.getElementById('text').innerHTML = "text";
-    document.getElementById('url').innerHTML = "url";
+    document.getElementById('title').value = "value";
+    document.getElementById('text').value = "text";
+    document.getElementById('url').value = "url";
   });
